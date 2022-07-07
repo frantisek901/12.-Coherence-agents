67779fabade5c82d931f79c7f560eecd3009605d
janlorenz <post@janlo.de>
2022-07-07 16:34:32 +0200

    First step Human Values

--- dataProcessing.R
+++ dataProcessing.R
@@ -36,3 +36,43 @@ glimpse(df)
 
 
 
+
+# Human values computation
+
+valuenames <- c("ipcrtiv", "imprich", "ipeqopt", "ipshabt", "impsafe", "impdiff", "ipfrule", "ipudrst", 
+  "ipmodst", "ipgdtim", "impfree", "iphlppl", "ipsuces", "ipstrgv", "ipadvnt", "ipbhprp",
+  "iprspot", "iplylfr", "impenv",  "imptrad", "impfun")  
+
+library(tidyverse)
+df_ten <- df |> select(ipcrtiv:impfun) |> rowwise() |> 
+  mutate(Conformity = mean(c_across(valuenames[c(7,16)])),
+         Tradition = mean(c_across(valuenames[c(9,20)])),
+         Benevolence = mean(c_across(valuenames[c(12,18)])),
+         Universalism = mean(c_across(valuenames[c(3,8,19)])),
+         SelfDirection = mean(c_across(valuenames[c(1,11)])),
+         Stimulation = mean(c_across(valuenames[c(6,15)])),
+         Hedonism = mean(c_across(valuenames[c(10,21)])),
+         Achievement = mean(c_across(valuenames[c(4,13)])),
+         Power = mean(c_across(valuenames[c(2,17)])),
+         Security = mean(c_across(valuenames[c(5,14)])),
+         mrat = mean(c_across(ipcrtiv:impfun)),
+         mrat_median = median(c_across(ipcrtiv:impfun))) |> 
+  ungroup()
+df_ten |> 
+  mutate(across(Conformity:Security, function(x) x - mrat)) 
+
+
+
+# Conformity       7,16
+# Tradition        9,20
+# Benevolence      12,18
+# Universalism     3,8,19
+# Self-Direction   1,11
+# Stimulation      6,15
+# Hedonism         10,21
+# Achievement      4,13
+# Power            2,17
+# Security         5,14
+
+
+
