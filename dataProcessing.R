c4d2cca92772d4f3b718193683bedb450ff9809d
Jan Lorenz <post@janlo.de>
2022-07-08 17:30:31 +0200

    Update dataProcessing.R

--- dataProcessing.R
+++ dataProcessing.R
@@ -83,3 +83,56 @@ table(df$euftf)
 table(df$imprich)
 table(df$impfun)
 # GREAT! File is clean!
+
+
+
+# Human values computation
+
+valuenames <- c("ipcrtiv", "imprich", "ipeqopt", "ipshabt", "impsafe", "impdiff", "ipfrule", "ipudrst", 
+  "ipmodst", "ipgdtim", "impfree", "iphlppl", "ipsuces", "ipstrgv", "ipadvnt", "ipbhprp",
+  "iprspot", "iplylfr", "impenv",  "imptrad", "impfun")  
+
+library(tidyverse)
+df_ten <- df |> select(idno, ipcrtiv:impfun) |> rowwise() |> 
+  mutate(Conformity = mean(c_across(valuenames[c(7,16)])),
+         Tradition = mean(c_across(valuenames[c(9,20)])),
+         Benevolence = mean(c_across(valuenames[c(12,18)])),
+         Universalism = mean(c_across(valuenames[c(3,8,19)])),
+         SelfDirection = mean(c_across(valuenames[c(1,11)])),
+         Stimulation = mean(c_across(valuenames[c(6,15)])),
+         Hedonism = mean(c_across(valuenames[c(10,21)])),
+         Achievement = mean(c_across(valuenames[c(4,13)])),
+         Power = mean(c_across(valuenames[c(2,17)])),
+         Security = mean(c_across(valuenames[c(5,14)])),
+         mrat = mean(c_across(ipcrtiv:impfun)),
+         mrat_median = median(c_across(ipcrtiv:impfun))) |> 
+  ungroup() |> 
+  mutate(across(Conformity:Security, function(x) x - mrat)) |> 
+  mutate(Openness = (SelfDirection + Stimulation)/2,
+         SelfEnhancement = (Hedonism + Achievement + Power)/3,
+         Conservation = (Security + Conformity + Tradition)/3,
+         SelfTranscendence = (Benevolence + Universalism)/2)
+df_four <- df_ten[sample(nrow(df_ten)),] |> 
+  select(idno, Openness:SelfTranscendence) |> 
+  pivot_longer(Openness:SelfTranscendence) |> 
+  group_by(idno) |> 
+  mutate(rank = rank(value)) |> 
+  group_by(idno) |> 
+  summarize(Value1 = name[value == max(value)][1], 
+            Value2 = name[value == min(value)][1],
+            Consistent =
+              str_starts(Value1,"Self") & str_starts(Value2,"Self") |
+              Value1 == "Openness" & Value2 == "Conservation" |
+              Value2 == "Openness" & Value1 == "Conservation") |> 
+  mutate(ValueType = if_else(Consistent, Value1, "Erratic"))
+
+mds <- df_ten |> select(Conformity:Security) |> t() |> dist() |> 
+  cmdscale(eig = TRUE, k =2)
+plot(mds$points[,1],mds$points[,2])
+text(mds$points[,1],mds$points[,2],labels = row.names(mds$points))
+mds <- df_ten |> select(Openness:SelfTranscendence) |> t() |> dist() |> 
+  cmdscale(eig = TRUE, k =2)
+plot(mds$points[,1],mds$points[,2])
+text(mds$points[,1],mds$points[,2],labels = row.names(mds$points))
+
+
